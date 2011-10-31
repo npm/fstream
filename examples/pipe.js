8048aa15759b2ac9b476be0b4511879a9f9a635e
isaacs <i@izs.me>
2011-10-31 15:41:48 -0700

    A pipe example

---
+++ examples/pipe.js
@@ -0,0 +1,28 @@
+var fstream = require("../fstream.js")
+var path = require("path")
+
+var r = fstream.Reader({ path: path.dirname(__dirname)
+                       , filter: function () {
+                           return !this.basename.match(/^\./) &&
+                                  !this.basename.match(/^node_modules$/)
+                                  !this.basename.match(/^deep-copy$/)
+                         }
+                       })
+
+var w = fstream.Writer({ path: path.resolve(__dirname, "deep-copy")
+                       , type: "Directory"
+                       })
+
+r.on("entry", function (entry) {
+  console.error("a %s appears!", entry.type, entry.path)
+})
+
+w.on("entry", function (entry) {
+  console.error("%s attacks!", entry.type, entry.path)
+})
+
+r.on("end", function () {
+  console.error("IT'S OVER!!")
+})
+
+r.pipe(w)
