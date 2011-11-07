b1560af935732bae68087c836a683f8fb9f3574d
isaacs <i@izs.me>
2011-11-06 16:49:37 -0800

    a more fun pipe example

--- examples/pipe.js
+++ examples/pipe.js
@@ -1,6 +1,6 @@
 var fstream = require("../fstream.js")
 var path = require("path")
-debugger
+
 var r = fstream.Reader({ path: path.dirname(__dirname)
                        , filter: function () {
                            return !this.basename.match(/^\./) &&
@@ -13,16 +13,83 @@ var w = fstream.Writer({ path: path.resolve(__dirname, "deep-copy")
                        , type: "Directory"
                        })
 
-r.on("entry", function (entry) {
-  console.error("a %s appears!", entry.type, entry.path)
-})
+var indent = ""
+var escape = {}
 
-w.on("entry", function (entry) {
-  console.error("%s attacks!", entry.type, entry.path)
-})
+r.on("entry", appears)
+//r.on("ready", function () {
+//  appears(r)
+//})
 
+function appears (entry) {
+  console.error(indent + "a %s appears!", entry.type, entry.basename)
+  indent += "\t"
+  entry.on("data", missile(entry))
+  entry.on("end", runaway(entry))
+  entry.on("entry", appears)
+}
+
+var foggy
+function missile (entry) { return function (c) {
+  if (entry.type === "Directory") {
+    if (!foggy && Math.random() < 0.3) {
+      console.error(indent +"%s casts a spell", entry.basename)
+      console.error("\na fog comes over the battlefield...\n\033[32m")
+      r.pause()
+      r.__resume = r.resume
+      r.resume = r.pause
+      w.once("drain", function f () {
+        if (foggy) {
+          console.error("\ntrying to lift the fog!\n")
+          r.pause()
+          w.once("drain", f)
+        }
+      })
+      foggy = setTimeout(function () {
+        console.error("\n\033[mthe fog lifts!\n")
+        setTimeout(function () {
+          foggy = null
+          r.resume = r.__resume
+          r.resume()
+        }, 300)
+      }, 1000)
+    }
+    return
+  }
+  var e = Math.random() < 0.5
+  console.error(indent + "%s %s for %d damage!",
+              entry.basename,
+              e ? "is struck" : "fires a chunk",
+              c.length)
+}}
+
+function runaway (entry) { return function () {
+  var e = Math.random() < 0.5
+  console.error(indent + "%s %s",
+                entry.basename,
+                e ? "turns to flee" : "is vanquished!")
+  indent = indent.slice(0, -1)
+}}
+
+
+w.on("entry", attacks)
+//w.on("ready", function () { attacks(w) })
+function attacks (entry) {
+  console.error(indent + "%s %s!", entry.basename,
+              entry.type === "Directory" ? "calls for backup" : "attacks")
+  entry.on("entry", attacks)
+}
+
+ended = false
 r.on("end", function () {
-  console.error("IT'S OVER!!")
+  if (foggy) clearTimeout(foggy)
+  console.error("\033[mIT'S OVER!!")
+  console.error("A WINNAR IS YOU!")
+  ended = true
 })
 
+process.on("exit", function () {
+  console.error("ended? "+ended)
+})
 r.pipe(w)
+
