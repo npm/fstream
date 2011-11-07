aab9ad4868fcf950e5d658f3e3d9f04fe3542723
isaacs <i@izs.me>
2011-11-06 16:58:59 -0800

    Piping from a dir to another dir works now, or from a tarball.

--- fstream.js
+++ fstream.js
@@ -1,2 +1,28 @@
 exports.Reader = require("./lib/reader.js")
 exports.Writer = require("./lib/writer.js")
+
+exports.File =
+  { Reader: require("./lib/file-reader.js")
+  , Writer: require("./lib/file-writer.js") }
+
+exports.Dir = 
+  { Reader : require("./lib/dir-reader.js")
+  , Writer : require("./lib/dir-writer.js") }
+
+exports.Link =
+  { Reader : require("./lib/link-reader.js")
+  , Writer : require("./lib/link-writer.js") }
+
+exports.Proxy =
+  { Reader : require("./lib/proxy-reader.js")
+  , Writer : require("./lib/proxy-writer.js") }
+
+exports.Reader.Dir = exports.Dir.Reader
+exports.Reader.File = exports.File.Reader
+exports.Reader.Link = exports.Link.Reader
+exports.Reader.Proxy = exports.Proxy.Reader
+
+exports.Writer.Dir = exports.Dir.Writer
+exports.Writer.File = exports.File.Writer
+exports.Writer.Link = exports.Link.Writer
+exports.Writer.Proxy = exports.Proxy.Writer
